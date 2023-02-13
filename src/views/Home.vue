abe9680835d961cae4d4975def313ec02304fcaa
Özgür Polat <ozgrplt@gmail.com>
2023-02-13 21:27:11 +0100

    final

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis laudantium omnis soluta dolorum, voluptates, nostrum, dolorem debitis ipsam incidunt dignissimos sint harum velit itaque! Consequuntur, atque nobis qui quam perspiciatis harum! Repudiandae, aperiam. Minima deleniti, cupiditate aspernatur quas dicta earum ullam voluptatem a dolor quaerat, dolores aperiam nihil voluptas eligendi autem! Eum natus sed laboriosam deleniti, quisquam quos voluptatum optio expedita consectetur corporis voluptate facere? Quae saepe culpa, quam corporis facilis laborum magnam voluptates inventore dolor voluptatem et ad ea iste beatae ullam, expedita iusto? Et accusamus recusandae aliquam, aliquid iusto, quod doloremque iste sint quam quidem temporibus inventore dolorem?</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis laudantium omnis soluta dolorum, voluptates, nostrum, dolorem debitis ipsam incidunt dignissimos sint harum velit itaque! Consequuntur, atque nobis qui quam perspiciatis harum! Repudiandae, aperiam. Minima deleniti, cupiditate aspernatur quas dicta earum ullam voluptatem a dolor quaerat, dolores aperiam nihil voluptas eligendi autem! Eum natus sed laboriosam deleniti, quisquam quos voluptatum optio expedita consectetur corporis voluptate facere? Quae saepe culpa, quam corporis facilis laborum magnam voluptates inventore dolor voluptatem et ad ea iste beatae ullam, expedita iusto? Et accusamus recusandae aliquam, aliquid iusto, quod doloremque iste sint quam quidem temporibus inventore dolorem?</p>  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
